19c223a78db21ee616f70928550a87e912e69efb
Rogue Blue <rogueblueprint@protonmail.com>
2022-01-30 13:32:56 -0700

    Lesson 3

--- dom.js
+++ dom.js
@@ -136,28 +136,116 @@ var itemList = document.querySelector('#items');
 // createElement
 
 // Create a div
-var newDiv =  document.createElement('div');
+// var newDiv =  document.createElement('div');
 
 // Add class
-newDiv.className= 'hello';
+// newDiv.className= 'hello';
 
 // Add id
-newDiv.id = 'hello1';
+// newDiv.id = 'hello1';
 
 // Add attr
-newDiv.setAttribute('title', 'Hello Div');
+// newDiv.setAttribute('title', 'Hello Div');
 
 // Create text node
-var newDivText = document.createTextNode('Hello World');
+// var newDivText = document.createTextNode('Hello World');
 
 // Add text to div
-newDiv.appendChild(newDivText);
+// newDiv.appendChild(newDivText);
 
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
 
-console.log(newDiv);
+// console.log(newDiv);
 
-newDiv.style.fontSize = '30px';
+// newDiv.style.fontSize = '30px';
 
-container.insertBefore(newDiv, h1);
+// container.insertBefore(newDiv, h1);
+
+
+// EVENTS
+
+// let button = document.getElementById('button').addEventListener
+// ('click', function(){
+//    console.log(123);
+//});
+
+// let button = document.getElementById('button').addEventListener
+// ('click', buttonClick);
+
+// function buttonClick(e){
+    //console.log('Button clicked');
+    //document.getElementById('header-title').textContent = "Changed";
+    //document.querySelector('#main').style.backgroundColor = '#f4f4f4';
+    //console.log(e);
+    //console.log(e.target);
+    //console.log(e.target.id);
+    //console.log(e.target.className);
+    //console.log(e.target.classList);
+    //let output = document.getElementById('output');
+    //output.innerHTML = '<h3>'+e.target.id+'</h3>';
+    
+    //console.log(e.type);
+
+    //console.log(e.clientX);
+    //console.log(e.clientY);
+
+    //console.log(e.offsetX);
+    //console.log(e.offsetY);
+
+    //console.log(e.altKey);
+    //console.log(e.ctrlKey);
+    //console.log(e.shiftKey);
+//}
+
+let button = document.getElementById('button');
+let box = document.getElementById('box');
+
+//button.addEventListener('click', runEvent);
+//button.addEventListener('dblclick', runEvent);
+//button.addEventListener('mousedown', runEvent);
+//button.addEventListener('mouseup', runEvent);
+
+//box.addEventListener('mouseenter', runEvent);
+//box.addEventListener('mouseleave', runEvent);
+
+//box.addEventListener('mouseover', runEvent);
+//box.addEventListener('mouseout', runEvent);
+
+//box.addEventListener('mousemove', runEvent);
+
+let itemInput = document.querySelector('input[type="text"]');
+let form = document.querySelector('form');
+let select = document.querySelector('select');
+
+//itemInput.addEventListener('keydown', runEvent);
+//itemInput.addEventListener('keyup', runEvent);
+//itemInput.addEventListener('keypress', runEvent);
+
+//itemInput.addEventListener('focus', runEvent);
+//itemInput.addEventListener('blur', runEvent);
+
+//itemInput.addEventListener('cut', runEvent);
+//itemInput.addEventListener('paste', runEvent);
+
+//itemInput.addEventListener('input', runEvent);
+
+//select.addEventListener('change', runEvent);
+//select.addEventListener('input', runEvent);
+
+//form.addEventListener('submit', runEvent);
+
+//function runEvent(e){
+    //e.preventDefault();
+    //console.log('EVENT TYPE: ' +e.type);
+
+    //document.body.style.display = 'none';
+
+    //console.log(e.target.value);
+    //document.getElementById('output').innerHTML = '<h3>' +e.target.value+ '</h3>';
+    //output.innerHTML = '<h3>MouseX: '+e.offsetX+' </h3><h3>MouseY: '+e.offsetY+'</h3>';
+
+    //box.style.backgroundColor = "rgb("+e.offsetX+", "+e.offsetY+", 40)";
+
+    //document.body.style.backgroundColor = "rgb("+e.offsetX+", "+e.offsetY+", 40)";
+//}
